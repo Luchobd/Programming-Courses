4aa235f974e3b3e0784ce05af97e4f59dfdc0f1b
Luis Blanco <luiswblancod@gmail.com>
2026-03-23 17:46:55 -0500

    clean architecture code

--- Flutter/04_toktik/lib/main.dart
+++ Flutter/04_toktik/lib/main.dart
@@ -2,22 +2,30 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_posts_repository_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository = VideoPostsRepositoryImpl(
+      videosDataSource: LocalVideoDataSource(),
+    );
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           lazy: false,
-          create: (_) => DiscoverProvider()..loadNextPage()
-        )
+          create: (_) =>
+              DiscoverProvider(videoPostRepository: videoPostRepository)
+                ..loadNextPage(),
+        ),
       ],
       child: MaterialApp(
         title: 'TokTik',
---
+++ Flutter/04_toktik/notes/clean_architecture.md
@@ -0,0 +1,11 @@
+# Clean Architecture
+
+1. UI
+
+2. Presentación
+
+<!-- 3. Casos de uso "NO SE APLICA PARA ESTE CURSO"--> 
+
+4. Repositorios y datasources
+
+5. Información regresa al UI
