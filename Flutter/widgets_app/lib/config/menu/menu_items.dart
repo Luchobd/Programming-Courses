259950cd2a6ef46a740045a40402656d25c9b8f0
Luis Blanco <luiswblancod@gmail.com>
2026-03-25 21:46:05 -0500

    modify and add also list and create home screen

---
+++ Flutter/widgets_app/lib/config/menu/menu_items.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: 'Botones',
+    subTitle: 'Botones en Flutter',
+    link: '/buttons',
+    icon: Icons.smart_button,
+  ),
+
+  MenuItem(
+    title: 'Tarjetas',
+    subTitle: 'Tarjetas en Flutter',
+    link: '/cards',
+    icon: Icons.credit_card,
+  ),
+];
